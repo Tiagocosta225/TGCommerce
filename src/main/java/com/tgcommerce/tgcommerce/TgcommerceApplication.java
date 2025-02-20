114a46d2081c1abf2fe4ecefe5dff5d11778179e
Tiago <Tiago@URBTI-004.urbanobank.corp>
2025-02-20 12:56:19 -0300

    Entidade User, banco H2

---
+++ src/main/java/com/tgcommerce/tgcommerce/TgcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.tgcommerce.tgcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgcommerce/tgcommerce/TgcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgcommerce.tgcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
