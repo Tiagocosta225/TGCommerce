90f1d3777131863af93ff66ad00eb44acddf029f
Tiagocosta225 <tiagocosta225@gmail.com>
2025-02-25 17:24:15 -0300

    Order, Enum, relacionamento muitos-para-um

--- src/main/java/com/tgcommerce/tgcommerce/entities/User.java
+++ src/main/java/com/tgcommerce/tgcommerce/entities/User.java
@@ -3,6 +3,8 @@ package com.tgcommerce.tgcommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -17,6 +19,9 @@ public class User {
     private LocalDate birthDate;
     private String password;
 
+    @OneToMany (mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
     public User(){
 
     }
@@ -28,6 +33,7 @@ public class User {
         this.phone = phone;
         this.birthDate = birthDate;
         this.password = password;
+
     }
 
     public Long getId() {
@@ -77,4 +83,9 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
 }
+
