b350ff906a514eb087c8e7625d50ae238c3843b4
Tiagocosta225 <tiagocosta225@gmail.com>
2025-03-06 18:41:49 -0300

    Muitos-para-muitos, column unique e text

--- src/main/java/com/tgcommerce/tgcommerce/entities/User.java
+++ src/main/java/com/tgcommerce/tgcommerce/entities/User.java
@@ -14,6 +14,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(unique = true)
+
     private String email;
     private String phone;
     private LocalDate birthDate;
